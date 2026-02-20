c92ad4b3b2b30e7643ef05548044499593c1a704
xpchen <xpchen9999@gmail.com>
2026-02-20 21:25:46 +0800

    Initial Commit

---
+++ HoopReel/HoopReelApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoopReelApp.swift
+//  HoopReel
+//
+//  Created by 陈晓鹏 on 2026/2/20.
+//
+
+import SwiftUI
+
+@main
+struct HoopReelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
